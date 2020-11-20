2ea87288e184b0a0f3457fec51841f774079127b
KYH <joker607@naver.com>
2020-11-20 21:57:04 +0900

    1

--- models/User.js
+++ models/User.js
@@ -1,33 +1,54 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name : {
+    UserID : {
         type : String,
-        maxlength : 50
+        maxlength : 20,
+       // unique : true,
+        //require : true
+    },
+    
+    Password: {
+        type : String,
+        minlength: 5,
+        //require : true
+    },
+    Name : {
+        type : String,
+        maxlength : 50,
+       // require : true
+    },
+    PhoneNum : {
+        type : Number,
+        maxlength : 11,
+        //require : true
+    },
+    Address : {
+        type : String,
+        maxlength : 30,
+        //require : true
+    },
+    Position : {
+        type: String,
+        default: "입주민",
+        //require : true
+    },
+    State : {
+        type : Number,
+        default : 0,
+        //require : true
+    },
+    Term : {
+        type : Date
+    },
+    ReApp : {
+        type : Number,
+        default : 0
     },
     email:{
         type : String,
         trim : true,
         unique: 1
-    },
-    password: {
-        type : String,
-        minlength: 5
-    },
-    lastname: {
-        type : String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token:{
-        type: String
-    },
-    tokenExp:{
-        type: Number
     }
 })
 
