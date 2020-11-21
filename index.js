f6c289d64d2c49f5ce4c8e39c559f383481998bb
KYH <joker607@naver.com>
2020-11-21 17:09:35 +0900

    .

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const config = require('./config/key')
 
 
 const { User } = require("./models/User")
+const { Vote } = require("./models/Vote")
 
 app.use(bodyParser.urlencoded({extended:true}));
 
@@ -24,7 +25,7 @@ mongoose.connect(config.mongoURI, {
 .catch(err=> console.log(err))
 
 
-app.post('/register',(req,res)=>{
+app.post('/api/users/register',(req,res)=>{
   //회원 가입 할때 필요한 정보들을 client에서 가저오면 그것들을 DB에 저장한다.
   const user = new User(req.body)
   
@@ -37,12 +38,11 @@ app.post('/register',(req,res)=>{
 })
 
 
+
+
 app.post('/api/users/login', (req, res) => {
   
   //요청된 아이디를 DB에서 찾기.
-  
-  
-
   User.findOne({ UserID : req.body.UserID}, (err, user) => {
     if(!user){
       return res.json({
@@ -50,12 +50,11 @@ app.post('/api/users/login', (req, res) => {
         message : "아이디를 다시 확인해주세요."
       })
     }
+    
     //요청된 아이디가 DB에 있다면 비밀번호가 맞는지 확인
     user.comparePassword(req.body.Password , (err, isMatch) => {
       if(!isMatch)
       return res.json({loginSuccess : false, message : "비밀번호를 다시 확인해주세요."})
-
-
 
     //비밀번호가 맞다면 토큰을 생성하기
       user.generateToken((err, user) => {
@@ -75,6 +74,14 @@ app.post('/api/users/login', (req, res) => {
 
 })
 
+
+  app.post('/api/vote', (req, res) =>{
+    if(err) return res.json({
+      make_Vote : false,
+      message : "투표생성 실패."
+})
+
+
 app.get("/", (req, res) => {
 
   res.send("Hello World!, 이김팀 테스트2");
@@ -88,4 +95,6 @@ app.listen(port, () => {
 
   console.log(`Example app listening at http://localhost:${port}`);
   
+  })
+
 });
---
+++ models/Vote.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
+const { isError } = require('util');
+
+const voteSchema = mongoose.Schema({
+    VoteID : {
+        type : String,
+        maxlength : 10,
+       // unique : true,
+        //require : true
+    },
+    
+    StartDay: {
+        type : Date
+        //require : true
+    },
+    EndDay : {
+        type : Date
+       // require : true
+    },
+    VoteHead : {
+        type : String,
+        maxlength : 100,
+        //require : true
+    },
+    VoteType : {
+        type : Number
+        //require : true
+        // 0은 입주민대표회의 임원, 1은 입주민대상, 2는 선관위 내부
+    },
+    MultCho : {
+        type: Boolean
+        //require : true
+        // True : 복수선택가능 False : 복수선택 불가능
+    },
+    Selc1 : {
+        type : String,
+        maxlength : 100
+        //require : true
+    },
+    Selc2 : {
+        type : String,
+        maxlength : 100
+        //require : true
+    }
+})
+
+
+
+module.exports = { Vote }
