c972688824e3649f95ca3a346609291d6db378c8
KYH <joker607@naver.com>
2020-11-26 19:15:24 +0900

    투표 1차 성공

--- Vote/VoteProject/vote/urls.py
+++ Vote/VoteProject/vote/urls.py
@@ -3,8 +3,8 @@ from .views import * #views에 정의한 함수 모두를 선언해준다.
 app_name='vote'
 
 urlpatterns = [
-    path('', index), #해당 path에는 ''를 입력했지만 실제로는 vote/라는 뜻
+    path('', index,name='index'), #해당 path에는 ''를 입력했지만 실제로는 vote/라는 뜻
     path('<int:qid>',detail, name='detail'),
     path('vote/', vote, name='vote'),
-    path('<int:q_id>/result/', result, name='result'),
+    path('result/<int:q_id>/', result, name="result"),
     ]
