0d4b5e86914f49ed2aba95ae88ca8378525791ba
KYH <joker607@naver.com>
2020-11-27 23:10:29 +0900

    로그인 접근 구현

--- Vote/VoteProject/VoteProject/settings.py
+++ Vote/VoteProject/VoteProject/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls.base import reverse_lazy 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_URL = reverse_lazy('login:signin')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
--- Vote/VoteProject/vote/views.py
+++ Vote/VoteProject/vote/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Question, Choice
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required #login_required를 선언하고
+
 
 # Create your views here.
 
 def index(request):
     a = Question.objects.all()
     return render(request, 'vote/index.html', {'a':a})
+
+@login_required
 
 def detail(request, qid):
     b=get_object_or_404(Question, id=qid)
@@ -24,3 +28,17 @@ def vote(request):
 
 def result(request, q_id):
     return render(request,'vote/result.html',{'q':get_object_or_404(Question,id=q_id)})
+
+
+def qregister(request):
+    if request.method == "GET":
+        form = QuestionForm()
+        return render(request, 'vote/qregister.html', {'f':form})
+    
+    elif request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            q = form.save(commit=False)
+            q.date = datetime.now()
+            q.save()
+            return HttpResponseRedirect(reverse('vote:index'))
